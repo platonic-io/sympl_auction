a19a7d00f7bd11fc921ce9b8712a725952cfadc2
Lukasz Dobrek <lukasz.dobrek@symbiont.io>
2022-08-24 09:35:04 +0200

    - clean up changes

--- lang10/scale_tests/auctions_scale_test.py
+++ lang10/scale_tests/auctions_scale_test.py
@@ -10,19 +10,9 @@ price_str="{price}"
 i = 0
 
 
-def async_call(  network, key_alias, contract_ref, function, kwargs):
-        """
-        calls the specified contract function on a node
-        :param key_alias: key_alias to invoke as
-        :param contract_ref: contract to call
-        :param function: function to invoke
-        :param kwargs: dictionary of arguments to the contract, will be json serialized
-        :return: the result of the call as a value or job object
-        """
+def async_call(network, key_alias, contract_ref, function, kwargs):
         session = network.node_sessions[network.alias_locations[key_alias]]
-
-        # if this clientside does not post, it'll be the value read, otherwise a job
-        result = node_api_client.call(
+        return node_api_client.call(
             session,
             key_alias,
             contract_ref,
@@ -31,28 +21,25 @@ def async_call(  network, key_alias, contract_ref, function, kwargs):
             query_tx_index=network.query_tx_index,
         )
 
+def wait_till(cond):
+  keep_waiting = cond()
+  while keep_waiting: 
+    time.sleep(0.01)
+    keep_waiting = cond()
+   
+
 def do_an_auction(auction, key_alias, bidder, second_bidder, other_key_alias, third_key_alias, network):
+    
     new_auction = auction.create_auction(product_description='stamps collection',
                                          initial_price='100',
                                          silent=True)
-
     auction.add_member(new_member=third_key_alias, id=new_auction['id'])
     auction.add_member(new_member=other_key_alias, id=new_auction['id'])
     max_n = 20
     for idx in range(1, max_n * 2):
-        if idx < max_n:
-          async_call(network, other_key_alias, AUCTION, "bid", {"id": new_auction['id'], "amount" :price_str.format(price=100+idx)})
-        else:
-        #bidder.bid(id=new_auction['id'], amount=price_str.format(price=100+idx))
-#    for idx in range(max_n+2, max_n*2):
-         async_call(network,third_key_alias, AUCTION, "bid", {"id": new_auction['id'], "amount" :price_str.format(price=100+idx)})
-#        second_bidder.bid(id=new_auction['id'], amount=price_str.format(price=100+idx))
-    keep_waiting = True
-    while keep_waiting: 
-      time.sleep(0.005)
-      # import pdb
-      # pdb.set_trace()
-      keep_waiting = not(str(auction.last_bid_for_auction(id=new_auction["id"])) == price_str.format(price=100 + (max_n*2) -1))
+      bidder = other_key_alias if idx < max_n else third_key_alias
+      async_call(network, bidder, AUCTION, "bid", {"id": new_auction['id'], "amount" :price_str.format(price=100+idx)})
+    wait_till(lambda  : not(str(auction.last_bid_for_auction(id=new_auction["id"])) == price_str.format(price=100 + (max_n*2) -1)))
     winning_bid = auction.close_auction(id=new_auction['id'])
     assert winning_bid['amount'] == price_str.format(price=100 + (max_n*2) -1)
 
