5e6a82b61b9a0d9612c04ed4b37cc98322760fe1
Brian Shirai <brian.shirai@symbiont.io>
2022-08-24 07:03:56 -0700

    Rename contract and disable admin check.

--- lang10/scale_tests/auctions_scale_test.py
+++ lang10/scale_tests/auctions_scale_test.py
@@ -2,7 +2,7 @@ import pytest
 import time
 from assembly_client.api.contracts import ContractRef
 
-AUCTION = ContractRef('auction', '1.0.0', 10)
+AUCTION = ContractRef('auction_bshirai', '1.0.0', 10)
 
 name_str = "stamps collection #{index}"
 price_str="{price}"
@@ -26,8 +26,9 @@ class TestScaleAuction():
 
     @pytest.fixture
     def reset_publish(self, network):
-        network.reset(sympl_version=10)
-        network.publish([AUCTION])
+        # network.reset(sympl_version=10)
+        # network.publish([AUCTION])
+        pass
 
     @pytest.fixture
     def key_alias(self, network):
@@ -43,17 +44,17 @@ class TestScaleAuction():
 
     @pytest.fixture
     def auction(self, network, reset_publish, key_alias):
-        return network[key_alias].auction['10-1.0.0']
+        return network[key_alias].auction_bshirai['10-1.0.0']
 
     @pytest.fixture
     def bidder(self, network, reset_publish, other_key_alias):
-        return network[other_key_alias].auction['10-1.0.0']
+        return network[other_key_alias].auction_bshirai['10-1.0.0']
 
     @pytest.fixture
     def second_bidder(self, network, reset_publish, third_key_alias):
-        return network[third_key_alias].auction['10-1.0.0']
+        return network[third_key_alias].auction_bshirai['10-1.0.0']
 
     #### Scale auction tests
     def test_scale_auctions_creation(self, auction, key_alias, bidder, second_bidder, other_key_alias, third_key_alias, benchmark):
         auction.create_admin(new_admin=key_alias)
-        benchmark.pedantic(do_an_auction, args=(auction, key_alias, bidder, second_bidder, other_key_alias, third_key_alias), iterations=3, rounds=3)
+        benchmark.pedantic(do_an_auction, args=(auction, key_alias, bidder, second_bidder, other_key_alias, third_key_alias), iterations=10, rounds=10)
